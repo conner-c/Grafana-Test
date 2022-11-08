e8524bd106cb901e649c2761fa8cb871814a3592
Conner <cook.r.conner@gmail.com>
2022-11-08 17:29:52 -0600

    Start of the URL shortener

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import req from 'express/lib/request';
 import { collectDefaultMetrics, register } from 'prom-client';
 
 collectDefaultMetrics();
@@ -13,5 +14,33 @@ app.get('/metrics', async (_req, res) => {
     res.status(500).end(err);
   }
 });
+
+app.get('/urlShortener', async (_req, res) => {
+
+});
+
+var ShortURL = new function() {
+
+	var _alphabet = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_',
+		_base = _alphabet.length;
+
+	this.encode = function(num) {
+		var str = '';
+		while (num > 0) {
+			str = _alphabet.charAt(num % _base) + str;
+			num = Math.floor(num / _base);
+		}
+		return str;
+	};
+
+	this.decode = function(str) {
+		var num = 0;
+		for (var i = 0; i < str.length; i++) {
+			num = num * _base + _alphabet.indexOf(str.charAt(i));
+		}
+		return num;
+	};
+
+};
 
 app.listen(4001, '0.0.0.0');
